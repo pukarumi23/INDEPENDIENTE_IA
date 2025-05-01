be3870d1396c2e996e2e2867f5ba384d3fdace4c
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-01 12:24:50 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, args }) => {
       const res = await fetch(args[0])
       buffer = await res.buffer()
     } else {
-      return conn.reply(m.chat,'[ ✰ ] Responde a una *imagen o video*.', m, rcanal)
+      return conn.reply(m.chat,'[  ■ ] Responde a una *imagen o video*.', m, rcanal)
     }
     await m.react('🕓')
 
