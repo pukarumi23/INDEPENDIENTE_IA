8642d544258ffd2d12c16002165ba966c6bd2f04
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-02 00:14:32 -0500

    Update downloader-tiktok.js
    mar

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,7 +1,7 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args || !args[0]) return conn.reply(m.chat, '🚩 Ingresa un enlace del vídeo de TikTok junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://vm.tiktok.com/ZMrFCX5jf/`, m, rcanal)
+if (!args || !args[0]) return conn.reply(m.chat, '🔶 Ingresa un enlace del vídeo de TikTok junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://vm.tiktok.com/ZMrFCX5jf/`, m, rcanal)
     if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `Verifica que el link sea de TikTok`, m, rcanal).then(_ => m.react('✖️'))
   await m.react('🕓')
 try {
@@ -16,7 +16,7 @@ let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n'
     txt += `	✩  *Compartidos* : ${share}\n`
     txt += `	✩  *Publicado* : ${published}\n`
     txt += `	✩  *Descargas* : ${downloads}\n\n`
-    txt += `> 🚩 *${textbot}*`
+    txt += `> 🔶 *${textbot}*`
 await conn.sendFile(m.chat, dl_url, 'tiktok.mp4', txt, m, null, rcanal)
 await m.react('✅')
 } catch {
