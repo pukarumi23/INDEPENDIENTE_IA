fc04b94b0a14f43adc1753fa55c9230427d0f66a
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-01 23:34:24 -0500

    Update afk-afk.js
    bbbb

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,7 +2,7 @@ let handler = async (m, { text, conn }) => {
     let user = global.db.data.users[m.sender]
     user.afk = + new Date
     user.afkReason = text
-    await conn.reply(m.chat, `❇️ Ahora estás ausente hasta que vuelvas a enviar un nuevo mensaje, cuando te intenten tagear el usuario será notificado de tu ausencia con el motivo.\n\n*${conn.getName(m.sender)}* Esta AFK, Motivo *${text ? ': ' + text : ''}*`, m, rcanal)
+    await conn.reply(m.chat, `❇🔶 Ahora estás ausente hasta que vuelvas a enviar un nuevo mensaje, cuando te intenten tagear el usuario será notificado de tu ausencia con el motivo.\n\n*${conn.getName(m.sender)}* Esta AFK, Motivo *${text ? ': ' + text : ''}*`, m, rcanal)
 }
 handler.help = ['afk *<razón>*']
 handler.tags = ['fun']
