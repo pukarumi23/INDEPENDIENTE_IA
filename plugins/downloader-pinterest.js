291a1884df50d7d6203d4e6bb9826a2c4f26a763
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-02 00:11:59 -0500

    Update downloader-pinterest.js
    ad

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,7 +1,7 @@
 import Starlights from '@StarlightsTeam/Scraper';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat,`🚩 Ingrese un enlace de Pinterest\n\nEjemplo:\n> *${usedPrefix + command}* https://ar.pinterest.com/pin/588142032613788991/`, m);
+if (!args[0]) return conn.reply(m.chat,`🔶 Ingrese un enlace de Pinterest\n\nEjemplo:\n> *${usedPrefix + command}* https://ar.pinterest.com/pin/588142032613788991/`, m);
 
 await m.react('🕓');
 try {
@@ -12,7 +12,7 @@ txt += `  ✩   *Calidad* : ${quality}\n`;
 txt += `  ✩   *Tamaño* : ${size}\n`;
 txt += `  ✩   *Duracion* : ${duration}\n`;
 txt += `  ✩   *Url* : ${url}\n\n`
-txt += `> 🚩 *${textbot}*`
+txt += `> 🔶 *${textbot}*`
 
 
 await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: txt, mimetype: 'video/mp4', fileName:  `pinterest.mp4`}, {quoted: m })
