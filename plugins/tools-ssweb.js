708e808ecdaec0cebdf3fcc02fcd49a8cfc1bd83
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 02:43:46 -0500

    nuevo

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, '🚩 Ingresa el enlace de la página.', m, rcanal)
+if (!args[0]) return conn.reply(m.chat, '🔶 Ingresa el enlace de la página.', m, rcanal)
 await m.react('🕓')
 try {
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
