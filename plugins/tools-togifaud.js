9fedabd0dd20d7630958d620905c7c1306596bfe
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 02:49:51 -0500

    nuevo

--- plugins/tools-togifaud.js
+++ plugins/tools-togifaud.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde a un *Video.*`, m, rcanal)
+if (!m.quoted) return conn.reply(m.chat, `🔶 Responde a un *Video.*`, m, rcanal)
 const q = m.quoted || m
 let mime = (q.msg || q).mimetype || ''
-if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🚩 Responde a un *Video.*`, m, rcanal)
+if (!/(mp4)/.test(mime)) return conn.reply(m.chat, `🔶 Responde a un *Video.*`, m, rcanal)
 await m.react('🕓')
 let media = await q.download()
 conn.sendMessage(m.chat, { video: media, gifPlayback: true, caption: listo }, { quoted: m })
