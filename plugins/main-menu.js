27362467e9395f7f2ecd4c7f0bbb10b09a67630a
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 01:00:14 -0500

    nuevo menu

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -25,7 +25,7 @@ const defaultMenu = {
   before: `
   *🔶♾️⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯♾️🔶*
 
-" Hola *%name* soy *[INDEPENDIENTE_BOT]*, %greeting "
+" Hola *%name* soy *INDEPENDIENTE_BOT*, %greeting "
 
 ╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*
 ┊ ‹‹ *Hello* :: *%name*
