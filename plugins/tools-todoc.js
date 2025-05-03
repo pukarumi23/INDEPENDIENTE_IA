b0da01ef7ce5694214e5990fab3246d7bd72245f
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 02:49:13 -0500

    nuevo

--- plugins/tools-todoc.js
+++ plugins/tools-todoc.js
@@ -3,9 +3,9 @@ import { toAudio } from '../lib/converter.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let q = m.quoted || m
 let mime = (q.msg || q).mimetype || ''
-if (!m.quoted) return conn.reply(m.chat,  `🚩 Etiqueta el *Video o Audio* que desea convertir en documento.`, m, rcanal)
-if(!text) return conn.reply(m.chat, `🚩 Ingresa el nombre para guardar el documento.`, m, rcanal)
-if (!/audio|video/.test(mime)) return conn.reply(m.chat,  `🚩 Etiqueta el *Video o Audio* que desea convertir en documento.`, m, rcanal)
+if (!m.quoted) return conn.reply(m.chat,  `🔶 Etiqueta el *Video o Audio* que desea convertir en documento.`, m, rcanal)
+if(!text) return conn.reply(m.chat, `🔶 Ingresa el nombre para guardar el documento.`, m, rcanal)
+if (!/audio|video/.test(mime)) return conn.reply(m.chat,  `🔶 Etiqueta el *Video o Audio* que desea convertir en documento.`, m, rcanal)
 let media = await q.download?.()
 if (!media) throw m.react('✖️')
 await m.react('🕓')
