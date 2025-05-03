b3279bf9b05e92f2cea05c0ff2fa0098a796f6dd
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 01:56:20 -0500

    nuevo diseño

--- plugins/rpg-deposit.js
+++ plugins/rpg-deposit.js
@@ -2,8 +2,8 @@
 
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *⭐ Estrellas* que deseas Depositar.')
-   if ((args[0]) < 1) return m.reply('🚩 Ingresa una cantidad válida de *⭐ Estrellas.')
+   if (!args[0]) return m.reply('🔶 Ingresa la cantidad de *⭐ Estrellas* que deseas Depositar.')
+   if ((args[0]) < 1) return m.reply('🔶 Ingresa una cantidad válida de *⭐ Estrellas.')
    if (args[0] == 'all') {
       let count = parseInt(user.limit)
       user.limit -= count * 1
@@ -11,7 +11,7 @@ let handler = async (m, { args }) => {
       await m.reply(`Depositaste *${count} ⭐ Estrellas* al Banco.`)
       return !0
    }
-   if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
+   if (!Number(args[0])) return m.reply('🔶 La cantidad deve ser un Numero.')
    let count = parseInt(args[0])
    if (!user.limit) return m.reply('No tienes *⭐ Estrellas* en la Cartera.')
    if (user.limit < count) return m.reply(`Solo tienes *${user.limit} ⭐ Estrellas* en la Cartera.`)
