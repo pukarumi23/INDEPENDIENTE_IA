d1ee01534f8098b28be54c23fa5280ba70eccefb
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 01:28:19 -0500

    nuevo diseño

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -5,10 +5,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) return m.reply(`🚩 Etiqueta a un usuario.`)
+    if (!who) return m.reply(`🔶 Etiqueta a un usuario.`)
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `🚩 @${who.split`@`[0]} ha sido baneado con exito, ya no podrá volver a usar mis comandos.`, m, { mentions: [who] })
+    conn.reply(m.chat, `🔶 @${who.split`@`[0]} ha sido baneado con exito, ya no podrá volver a usar mis comandos.`, m, { mentions: [who] })
 }
 handler.help = ['mban *@user*']
 handler.tags = ['owner']
