f54e46e872e221609026eee7e887d5ca94e5161c
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 01:41:50 -0500

    nuevo diseño

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) return m.reply(`🚩 Etiqueta a un usuario.`)
+    if (!who) return m.reply(`🔶 Etiqueta a un usuario.`)
     let users = global.db.data.users
     users[who].banned = false
-    conn.reply(m.chat, `🚩 @${who.split`@`[0]} ha sido desbaneado con exito, ahora podrá volver a usar mis comandos.`, m, { mentions: [who] })
+    conn.reply(m.chat, `🔶 @${who.split`@`[0]} ha sido desbaneado con exito, ahora podrá volver a usar mis comandos.`, m, { mentions: [who] })
 }
 handler.help = ['munban *@user*']
 handler.tags = ['owner']
