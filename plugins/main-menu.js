6100bd6b76f0f118d0a7048381cbbfe32cb5b436
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-02 22:40:58 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -23,11 +23,11 @@ let tags = {
 
 const defaultMenu = {
   before: `
-  *♾️🔆🔸⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯🔸🔆♾️*
+  *♾️🔆🔸⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯🔸🔆♾️*
 
 " Hola *%name* soy *INDEPENDIENTE_BOT*, %greeting "
 
-╭── ︿︿︿︿︿ ◈◈◈◈◈◈◈◈◈
+╭── ︿︿︿︿︿ ◈◈◈◈◈◈◈
 ┊ ‹‹ *Hello* :: *%name*
 ┊01 *INDEPENDIENTE_BOT*
 ╰─── independiente bot
