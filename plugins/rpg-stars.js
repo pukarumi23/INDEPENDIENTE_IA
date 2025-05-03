1b0353b74941bab9c83031a80caec80429b29376
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 02:00:22 -0500

    nuevo diseño

--- plugins/rpg-stars.js
+++ plugins/rpg-stars.js
@@ -5,7 +5,7 @@ let handler = async (m, {conn, usedPrefix}) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
     let name = conn.getName(who)
-    if (!(who in global.db.data.users)) return conn.reply(m.chat, '🚩 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
+    if (!(who in global.db.data.users)) return conn.reply(m.chat, '🔶 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
     let img = await (await fetch(`https://tinyurl.com/yrkgv3qm`)).buffer()
     let txt = ` –  *S T A R S  -  U S E R*\n\n`
         txt += `┌  ✩  *Nombre* : ${user.name}\n`
