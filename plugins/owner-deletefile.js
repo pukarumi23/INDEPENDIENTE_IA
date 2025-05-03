ed43529ea2d364ae8312f51b1f6c873f7b9c64c3
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 01:29:27 -0500

    nuevo diseño

--- plugins/owner-deletefile.js
+++ plugins/owner-deletefile.js
@@ -10,13 +10,13 @@ import {
 } from 'fs'
 
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args, text }) => {
-    if (!text) return conn.reply(m.chat, `🚩 Ingresa la ruta y el nombre del archivo que deseas eliminar.`, m, rcanal)
+    if (!text) return conn.reply(m.chat, `🔶 Ingresa la ruta y el nombre del archivo que deseas eliminar.`, m, rcanal)
     
     const file = text.trim()
-    if (!existsSync(file)) return conn.reply(m.chat, `🚩 Archivo no encontrado.`, m, rcanal)
+    if (!existsSync(file)) return conn.reply(m.chat, `🔶 Archivo no encontrado.`, m, rcanal)
     
     unlinkSync(file)
-    conn.reply(m.chat, `🚩 El archivo *${file}* ha sido eliminado con éxito.`, m, rcanal)
+    conn.reply(m.chat, `🔶 El archivo *${file}* ha sido eliminado con éxito.`, m, rcanal)
 }
 handler.tags = ['owner']
 handler.help = ['deletefile']
