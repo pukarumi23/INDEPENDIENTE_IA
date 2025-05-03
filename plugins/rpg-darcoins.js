a9c1329fff4a1281a341d44da012774209152eac
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 01:51:12 -0500

    nuevo diseño

--- plugins/rpg-darcoins.js
+++ plugins/rpg-darcoins.js
@@ -1,20 +1,18 @@
-
-
 import MessageType from '@whiskeysockets/baileys'
 let impuesto = 0.02
 let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw '🚩 Menciona al usuario con *@user.*'
+    if (!who) throw '🔶 Menciona al usuario con *@user.*'
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw '🚩 Ingrese la cantidad de *⭐ Estrellas* que quiere transferir.'
+    if (!txt) throw '🔶 Ingrese la cantidad de *⭐ Estrellas* que quiere transferir.'
     if (isNaN(txt)) throw 'Sólo números.'
     let poin = parseInt(txt)
     let limit = poin
     let imt = Math.ceil(poin * impuesto)
     limit += imt
-    if (limit < 1) throw '🚩 Mínimo es *1 ⭐ Estrella*.'
+    if (limit < 1) throw '🔶 Mínimo es *1 ⭐ Estrella*.'
     let users = global.db.data.users
     if (limit > users[m.sender].limit) throw 'No tienes suficientes *⭐ Estrellas* para dar.'
     users[m.sender].limit -= limit
