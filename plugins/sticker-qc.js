b0fcb9c94ec9077e00e38c451f336fabdfbe2364
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 02:19:10 -0500

    nuevo diseño

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -2,7 +2,7 @@ import { sticker } from '../lib/sticker.js'
 import axios from 'axios'
 
 let handler = async (m, { conn, text }) => {
-   if (!text) return conn.reply(m.chat, '🚩 Ingresa un texto junto al comando.', m, rcanal)
+   if (!text) return conn.reply(m.chat, '🔶 Ingresa un texto junto al comando.', m, rcanal)
    if (text.length > 30) return conn.reply(m.chat, 'Solo se permiten 30 caracteres como Máximo.', m, rcanal)
    await m.react('🕓')
    try {
