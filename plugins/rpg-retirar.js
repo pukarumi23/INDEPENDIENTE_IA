42c226724bbd4c62032bb7c7db58570de7e1228c
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-03 01:58:39 -0500

    nuevo diseño

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -2,15 +2,15 @@
 
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *⭐ Estrellas* que deseas Retirar.')
+   if (!args[0]) return m.reply('🔶 Ingresa la cantidad de *⭐ Estrellas* que deseas Retirar.')
    if (args[0] == 'all') {
       let count = parseInt(user.bank)
       user.bank -= count * 1
       user.limit += count * 1
-      await m.reply(`🚩 Retiraste *${count} ⭐ Estrellas* del Banco.`)
+      await m.reply(`🔶 Retiraste *${count} ⭐ Estrellas* del Banco.`)
       return !0
    }
-   if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
+   if (!Number(args[0])) return m.reply('🔶 La cantidad deve ser un Numero.')
    let count = parseInt(args[0])
    if (!user.bank) return m.reply('No tienes *⭐ Estrellas* en el Banco.')
    if (user.bank < count) return m.reply(`Solo tienes *${user.bank} ⭐ Estrellas* en el Banco.`)
