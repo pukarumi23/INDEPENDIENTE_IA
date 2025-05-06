6080828e7f3d6abc63e07607d9f201bba8742724
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-05 21:06:48 -0500

    new crimen

--- plugins/rpg-crimen.js
+++ plugins/rpg-crimen.js
@@ -36,7 +36,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
   users[senderId].limit += amountTaken
   users[randomUserId].limit -= amountTaken
   conn.sendMessage(m.chat, {
-        text: `🔶¡Lograste cometer tu crimen con exito!, acabas de robar *${amountTaken} ⭐ Estrellas* a @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} ⭐ Estrellas* a ${senderName}.`,
+        text: `🔶¡Lograste cometer tu crimen con exito!, acabas de robar *${amountTaken} ⭐ intis* a @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} ⭐ intis* a ${senderName}.`,
   contextInfo: { 
   mentionedJid: [randomUserId],
   }
@@ -46,7 +46,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
   case 1:
   let amountSubtracted = Math.min(Math.floor(Math.random() * (senderLimit - minAmount + 1)) + minAmount, maxAmount)
   users[senderId].limit -= amountSubtracted
-  conn.reply(m.chat, `🔶 No fuiste cuidadoso y te atraparon mientras cometias tu cirme, se restaron *-${amountSubtracted} ⭐ Estrellas* a ${senderName}.`, m)
+  conn.reply(m.chat, `🔶 No fuiste cuidadoso y te atraparon mientras cometias tu cirme, se restaron *-${amountSubtracted} ⭐ intis* a ${senderName}.`, m)
   break
 
   case 2:
@@ -54,7 +54,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
   users[senderId].limit += smallAmountTaken
   users[randomUserId].limit -= smallAmountTaken
   conn.sendMessage(m.chat, {
-  text: `🔶 Lograste cometer tu crimen con exito, pero te descubrieron y solo lograste tomar *${smallAmountTaken} ⭐ Estrellas* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} ⭐ Estrellas* a ${senderName}.`,
+  text: `🔶 Lograste cometer tu crimen con exito, pero te descubrieron y solo lograste tomar *${smallAmountTaken} ⭐ intis* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} ⭐ intis* a ${senderName}.`,
   contextInfo: { 
   mentionedJid: [randomUserId],
   }
