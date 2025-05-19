3361761bdeef8573bd736538e5e6de48481f6559
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-19 01:08:11 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -44,9 +44,9 @@ const defaultMenu = {
 *─ׄ─ׅ─ׄ─⭒ L I S T A  -  M E N Ú S ⭒─ׄ─ׅ─ׄ─*
 `.trimStart(),
   header: '╔═══◇◆🥬【 𝑴𝑬𝑵Ú メ %category 】🥬◆◇═══╗\n║╔───────────────────────',
-  body: '║🌱┊%cmd %islimit %isPremium\n',
-  footer: '║───────────────────────────\n╚═════════◆◇◆═════════╝\n',
-  after: `> 💙 ${textbot}`,
+    body: '║🌱┊%cmd %islimit %isPremium\n',
+   footer: '║───────────────────────────\n╚═════════◆◇◆═════════╝\n',
+    after: `> 💙 ${textbot}`,
 }
 
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
@@ -146,10 +146,27 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-
+    
+    let pp = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp2 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp3 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp4 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp5 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp6 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp7 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp8 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp9 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp10 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp11 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp12 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp13 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp14 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
+    let pp15 = 'https://telegra.ph/file/5e7042bf17cde23989e71.jpg'
     let img = `./storage/img/menu.jpg`
     await m.react('💙')
+   // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
+   //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
 
   } catch (e) {
     conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
@@ -163,6 +180,7 @@ handler.command = ['menu', 'help', 'menú']
 handler.register = true 
 export default handler
 
+
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
@@ -173,8 +191,8 @@ function clockString(ms) {
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 
-var ase = new Date();
-var hour = ase.getHours();
+  var ase = new Date();
+  var hour = ase.getHours();
 switch(hour){
   case 0: hour = 'una linda noche 🌙'; break;
   case 1: hour = 'una linda noche 💤'; break;
@@ -201,4 +219,4 @@ switch(hour){
   case 22: hour = 'una linda noche 🌙'; break;
   case 23: hour = 'una linda noche 🌃'; break;
 }
-var greeting = "espero que tengas " + hour;
+  var greeting = "espero que tengas " + hour;
