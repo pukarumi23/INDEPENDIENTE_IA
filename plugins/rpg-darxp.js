f36e70fed55e7b9946c3956f206648bfbadc08ed
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-22 02:29:10 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-darxp.js
+++ plugins/rpg-darxp.js
@@ -1,20 +1,18 @@
-
-
 import MessageType from '@whiskeysockets/baileys'
 let impuesto = 0.02
 let handler = async (m, { conn, text }) => {
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw '🚩 Menciona al usuario con *@user.*'
+  if (!who) throw '🔶 Menciona al usuario con *@user.*'
   let txt = text.replace('@' + who.split`@`[0], '').trim()
-  if (!txt) throw '🚩 Ingrese la cantidad de *💫 XP* que quiere transferir.'
+  if (!txt) throw '🔶 Ingrese la cantidad de *💫 XP* que quiere transferir.'
   if (isNaN(txt)) throw 'Sólo números.'
   let xp = parseInt(txt)
   let exp = xp
   let imt = Math.ceil(xp * impuesto)
   exp += imt
-  if (exp < 1) throw '🚩 Mínimo es 1 💫 XP.*'
+  if (exp < 1) throw '🔶 Mínimo es 1 💫 XP.*'
   let users = global.db.data.users
   if (exp > users[m.sender].exp) throw '*💫 XP* insuficiente para transferir.'
   users[m.sender].exp -= exp
