03d8062bef82bdb7458011cfb30af0a5f5fdd88f
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-22 02:57:57 -0500

    💙💙💙

--- plugins/rpg-intis.js
+++ plugins/rpg-intis.js
@@ -5,22 +5,20 @@ let handler = async (m, {conn, usedPrefix}) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
     let name = conn.getName(who)
-    if (!(who in global.db.data.users)) return conn.reply(m.chat, '🔶 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
-    let img = await (await fetch(`https://sl.bing.net/ebAy443aU9s`)).buffer()
-    let txt = ` –  *⫷ I N T I S ⫸ - ⫷ U S E R ⫸*\n\n`
-	txt += `🔆▰▰▰⩥🔥⩤▰▰▰🔆▶\n`
-        txt += `▮  🔶  *Nombre* : ${user.name}\n`
-        txt += `▮  🔶  *Estrellas* : ${toNum(user.limit)} ( *${user.limit}* )\n`
-        txt += `▮  🔶  *Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
-        txt += `▮  🔶  *XP* : ${toNum(user.exp)} ( *${user.exp}* )\n`
-	txt += `🔆▰▰▰⩥🔥⩤▰▰🔆▷`
+    if (!(who in global.db.data.users)) return conn.reply(m.chat, '💙 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
+    let img = await (await fetch(`https://w7.pngwing.com/pngs/531/1011/png-transparent-hatsune-miku-vocaloid-chibi-holography-anime-hatsune-miku-fictional-characters-manga-chibi.png`)).buffer()
+    let txt = ` –  *C E B O L L I N E S  -  U S E R*\n\n`
+        txt += `┌  💙  *Nombre* : ${user.name}\n`
+        txt += `│  💙  *Cebollines* : ${toNum(user.limit)} ( *${user.limit}* )\n`
+        txt += `│  💙  *Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
+        txt += `└  💙  *XP* : ${toNum(user.exp)} ( *${user.exp}* )`
     let mentionedJid = [who]
         
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 }
-handler.help = ['intis']
+handler.help = ['Cebollines']
 handler.tags = ['rpg']
-handler.command = ['coins', 'wallet', 'cartera', 'intis', 'soles', 'bal', 'balance']
+handler.command = ['coins', 'wallet', 'cartera', 'cebollines', 'cebollin', 'bal', 'balance']
 handler.register = true 
 export default handler
 
