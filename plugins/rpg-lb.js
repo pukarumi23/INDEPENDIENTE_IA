21e83f1b2acb484258853794d86a26d9a5d6da4a
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-22 03:50:30 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -9,12 +9,12 @@ let usersLim = sortedLim.map(enumGetKey)
 let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 let text = `
-▮▰▰▰[ 🔶 *Top ${len} 🪙 Intis* 🔶 ]▰▰▰◇▶
+▮▢◇▢◇[ 🔶 *Top ${len} 🪙 Intis* 🔶 ]▢◇▢◇▶
 ▮╭───────────────···
-⓵│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-⓺│ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} 🔆*`).join`\n🔶│ `}
+▮│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+▮│ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} 🔆*`).join`\n▮│ `}
 ▮╰────────────────···
-▮▰▰▰▰▰▰▰▰▰▰◇▶
+▮▢◇▢◇▢◇▢◇▢◇▢◇▢◇▶
 
 ╭───═[ 🔶 *TOP ${len} XP 💫* 🔶 ]═────⋆
 │╭───────────────···
