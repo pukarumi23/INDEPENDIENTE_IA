62ce249bba1364a5da91fcbfdec2d4c0b918c893
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-22 03:05:02 -0500

    🔶

--- plugins/rpg-intis.js
+++ plugins/rpg-intis.js
@@ -6,12 +6,14 @@ let handler = async (m, {conn, usedPrefix}) => {
     let user = global.db.data.users[who]
     let name = conn.getName(who)
     if (!(who in global.db.data.users)) return conn.reply(m.chat, '💙 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
-    let img = await (await fetch(`https://w7.pngwing.com/pngs/531/1011/png-transparent-hatsune-miku-vocaloid-chibi-holography-anime-hatsune-miku-fictional-characters-manga-chibi.png`)).buffer()
-    let txt = ` –  *C E B O L L I N E S  -  U S E R*\n\n`
-        txt += `┌  💙  *Nombre* : ${user.name}\n`
-        txt += `│  💙  *Cebollines* : ${toNum(user.limit)} ( *${user.limit}* )\n`
-        txt += `│  💙  *Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
-        txt += `└  💙  *XP* : ${toNum(user.exp)} ( *${user.exp}* )`
+    let img = await (await fetch(`https://sl.bing.net/kV8ahiwMMZE`)).buffer()
+    let txt = ` –  *⫷ I N T I S ⫸ - ⫷ U S E R ⫸*\n\n`
+	txt += `🔆▰▰▰⩥🔥⩤▰▰▰🔆\n`
+        txt += `▮  🔶  *Nombre* : ${user.name}\n`
+        txt += `▮  🔶  *Intis* : ${toNum(user.limit)} ( *${user.limit}* )\n`
+        txt += `▮  🔶  *Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
+        txt += `▮  🔶  *XP* : ${toNum(user.exp)} ( *${user.exp}* )`
+	txt += `🔆▰▰▰⩥🔥⩤▰▰▰🔆`
     let mentionedJid = [who]
         
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
