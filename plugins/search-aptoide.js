e0ec262d2a42c2f9f0bd0dd7e11a28c5b01d2069
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-21 23:51:27 -0500

    search apdoide 1

--- plugins/search-aptoide.js
+++ plugins/search-aptoide.js
@@ -1,18 +1,18 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-  if (!text) return conn.reply(m.chat, '[ ✰ ] Ingresa el nombre de la aplicación que deseas buscar en *Aptoide* junto al comando.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* WhatsApp`, m, rcanal)
+  if (!text) return conn.reply(m.chat, '🔶  Ingresa el nombre de la aplicación que deseas buscar en *Aptoide* junto al comando.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* WhatsApp`, m, rcanal)
   await m.react('🕓')
   try {
     let res = await Starlights.aptoideSearch(text)
     let img = `https://telegra.ph/file/e7eae20d14bf755fc4ebb.jpg`
-    let txt = `*乂  A P T O I D E  -  S E A R C H*`
+    let txt = `*🔶  ⫷ 𝘼𝙋𝙏𝙊𝙄𝘿𝙀 - 𝙎𝙀𝘼𝙍𝘾𝙃 ⫸  🔶 *`
     
     for (let i = 0; i < res.length; i++) {
       txt += `\n\n`
-      txt += `  *» Nro* : ${res[i].nro}\n`
-      txt += `  *» Nombre* : ${res[i].name}\n`
-      txt += `  *» ID* : ${res[i].id}`
+      txt += `  *🔶  Nro* : ${res[i].nro}\n`
+      txt += `  *🔶  Nombre* : ${res[i].name}\n`
+      txt += `  *🔶  ID* : ${res[i].id}`
     }
     
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
