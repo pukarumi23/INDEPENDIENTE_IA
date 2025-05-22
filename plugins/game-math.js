e5b7e104cf365b74104d3b29772673ae76ffcf46
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-22 01:08:52 -0500

    mates

--- plugins/game-math.js
+++ plugins/game-math.js
@@ -1,87 +1,90 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    conn.math = conn.math ? conn.math : {}
-    
-    if (args.length < 1) throw `
-  🧮 Dificultades disponibles : 
-  
-${Object.keys(modes).join(' | ')} 
+global.math = global.math ? global.math : {};
+const handler = async (m, {conn, args, usedPrefix, command}) => {
+  const mat =`
+*💙 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝙳𝙸𝙵𝙸𝙲𝚄𝙻𝚃𝙰𝙳𝙾 𝙲𝙾𝙽 𝙻𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙰 𝙹𝚄𝙶𝙰𝚁*
 
-_📌Ejemplo : ${usedPrefix+command} normal_
-`.trim()
-  
-  let mode = args[0].toLowerCase()
-  if (!(mode in modes)) throw `
-  🧮 Dificultades disponibles : 
-  
- ${Object.keys(modes).join(' | ')}
+*𝙳𝙸𝙵𝙸𝙲𝚄𝙻𝚃𝙰𝙳𝙴𝚂 𝙳𝙸𝚂𝙿𝙾𝙽𝙸𝙱𝙻𝙴𝚂: ${Object.keys(modes).join(' | ')}*
+*𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾: ${usedPrefix}mates medium*
+`.trim();
+  if (args.length < 1) return await conn.sendButton(m.chat, mat, wm, null, [
+['𝙼𝙰𝚃𝙴𝚂 𝙴𝙰𝚂𝚈', `${usedPrefix + command} easy`],
+['𝙼𝙰𝚃𝙴𝚂 𝙼𝙴𝙳𝙸𝚄𝙼', `${usedPrefix + command} medium`],
+['𝙼𝙰𝚃𝙴𝚂 𝙷𝙰𝚁𝙳', `${usedPrefix + command} hard`]], m, rcanal)
 
-_📌Ejemplo : ${usedPrefix+command} normal_
-`.trim()
-    
-  let id = m.chat
-    if (id in conn.math) return conn.reply(m.chat, '💙 Todavía hay preguntas sin respuesta en este chat', conn.math[id][0])
-    let math = genMath(mode)
-    conn.math[id] = [
-        await conn.reply(m.chat, `▢ CUANTO ES *${math.str}*=\n\n_Tiempo:_ ${(math.time / 1000).toFixed(2)} segundos\n\n🎁 Recompensa : ${math.bonus} XP`, m),
-        math, 4,
-        setTimeout(() => {
-            if (conn.math[id]) conn.reply(m.chat, `⏳ Se acabó el tiempo!\nLa respuesta es : *${math.result}*`, conn.math[id][0])
-      delete conn.math[id]
-        }, math.time)
-    ]
-}
+/*conn.sendHydrated(m.chat, mat, author, null, null, null, null, null, [
+['𝙼𝙰𝚃𝙴𝚂 𝙴𝙰𝚂𝚈', `${usedPrefix + command} easy`],
+['𝙼𝙰𝚃𝙴𝚂 𝙼𝙴𝙳𝙸𝚄𝙼', `${usedPrefix + command} medium`],
+['𝙼𝙰𝚃𝙴𝚂 𝙷𝙰𝚁𝙳', `${usedPrefix + command} hard`]], m)*/
 
-// Ayuda y comandos
-handler.help = ['mates']
-handler.tags = ['game']
-handler.command = ['mates', 'mate', 'matemáticas', 'math'] 
+  const mode = args[0].toLowerCase();
+  if (!(mode in modes)) return await conn.reply(m.chat, mat, m);
 
-// Modos de dificultad
-let modes = {
-    noob: [-3, 3, -3, 3, '+-', 15000, 100],
-    fácil: [-10, 10, -10, 10, '*/+-', 20000, 400],
-    normal: [-40, 40, -20, 20, '*/+-', 40000, 700],
-    difícil: [-100, 100, -70, 70, '*/+-', 30000, 800],
-    extremo: [-999999, 999999, -999999, 999999, '*/', 99999, 4500]
-}
+  /* conn.sendHydrated(m.chat, mat, author, null, null, null, null, null, [
+['𝙼𝙰𝚃𝙴𝚂 𝙴𝙰𝚂𝚈', `${usedPrefix + command} easy`],
+['𝙼𝙰𝚃𝙴𝚂 𝙼𝙴𝙳𝙸𝚄𝙼', `${usedPrefix + command} medium`],
+['𝙼𝙰𝚃𝙴𝚂 𝙷𝙰𝚁𝙳', `${usedPrefix + command} hard`]], m)*/
 
-// Operadores para mostrar
-let operators = {
-    '+': '+',
-    '-': '-',
-    '*': '×',
-    '/': '÷'
-}
+  const id = m.chat;
+  if (id in global.math) return conn.reply(m.chat, '*💙 𝚃𝙾𝙳𝙰𝚅𝙸𝙰 𝙷𝙰𝚈 𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰𝚂 𝚂𝙸𝙽 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃!*', global.math[id][0]);
+  const math = genMath(mode);
+  global.math[id] = [
+    await conn.reply(m.chat, `💙 𝙲𝚄𝙰𝙽𝚃𝙾 𝙴𝚂 𝙴𝙻 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴 *${math.str}*?\n\n*⏳ 𝚃𝙸𝙴𝙼𝙿𝙾: ${(math.time / 1000).toFixed(2)} 𝚜𝚎𝚐𝚞𝚗𝚍𝚘𝚜*\n*🏆 𝙶𝙰𝙽𝙰 𝙷𝙰𝚂𝚃𝙰: ${math.bonus} Cookies*`, m),
+    math, 4,
+    setTimeout(() => {
+      if (global.math[id]) {
+        conn.reply(m.chat, `*💙 𝚂𝙴 𝙰𝙷 𝙵𝙸𝙽𝙰𝙻𝙸𝚉𝙰𝙳𝙾 𝙴𝙻 𝚃𝙸𝙴𝙼𝙿𝙾 𝙿𝙰𝚁𝙰 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁*\n\n*𝙻𝙰 𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰 𝙴𝚂 ${math.result}*`, m),
+        // conn.sendButton(m.chat, `*💙 𝚂𝙴 𝙰𝙷 𝙵𝙸𝙽𝙰𝙻𝙸𝚉𝙰𝙳𝙾 𝙴𝙻 𝚃𝙸𝙴𝙼𝙿𝙾 𝙿𝙰𝚁𝙰 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁*\n\n*𝙻𝙰 𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰 𝙴𝚂 ${math.result}*`, wm, null, [['𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁', `${usedPrefix + command} ${math.mode}`]], global.math[id][0])
+        delete global.math[id];
+      }
+    }, math.time),
+  ];
+};
+handler.help = ['math <mode>'];
+handler.tags = ['fun'];
+handler.command = /^math|mates|matemáticas/i;
+handler.register = true;
+export default handler;
 
-// Generador de problemas matemáticos
+const modes = {
+  noob: [-3, 3, -3, 3, '+-', 15000, 1],
+  easy: [-10, 10, -10, 10, '*/+-', 20000, 4],
+  medium: [-40, 40, -20, 20, '*/+-', 40000, 15],
+  hard: [-100, 100, -70, 70, '*/+-', 60000, 35],
+  extreme: [-999999, 999999, -999999, 999999, '*/', 99999, 45],
+  impossible: [-99999999999, 99999999999, -99999999999, 999999999999, '*/', 30000, 50],
+  impossible2: [-999999999999999, 999999999999999, -999, 999, '/', 30000, 60],
+};
+
+const operators = {
+  '+': '+',
+  '-': '-',
+  '*': '×',
+  '/': '÷',
+};
+
 function genMath(mode) {
-    let [a1, a2, b1, b2, ops, time, bonus] = modes[mode]
-    let a = randomInt(a1, a2)
-    let b = randomInt(b1, b2)
-    let op = pickRandom([...ops])
-    let result = (new Function(`return ${a} ${op.replace('/', '*')} ${b < 0 ? `(${b})` : b}`))()
-    if (op == '/') [a, result] = [result, a]
-    return {
-        str: `${a} ${operators[op]} ${b}`,
-        mode,
-        time,
-        bonus,
-        result
-    }
+  const [a1, a2, b1, b2, ops, time, bonus] = modes[mode];
+  let a = randomInt(a1, a2);
+  const b = randomInt(b1, b2);
+  const op = pickRandom([...ops]);
+  let result = (new Function(`return ${a} ${op.replace('/', '*')} ${b < 0 ? `(${b})` : b}`))();
+  if (op == '/') [a, result] = [result, a];
+  return {
+    str: `${a} ${operators[op]} ${b}`,
+    mode,
+    time,
+    bonus,
+    result,
+  };
 }
 
-// Función para número aleatorio
 function randomInt(from, to) {
-    if (from > to) [from, to] = [to, from]
-    from = Math.floor(from)
-    to = Math.floor(to)
-    return Math.floor((to - from) * Math.random() + from)
+  if (from > to) [from, to] = [to, from];
+  from = Math.floor(from);
+  to = Math.floor(to);
+  return Math.floor((to - from) * Math.random() + from);
 }
 
-// Función para selección aleatoria
 function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)]
+  return list[Math.floor(Math.random() * list.length)];
 }
-
-// Exportar handler
-export default handler
