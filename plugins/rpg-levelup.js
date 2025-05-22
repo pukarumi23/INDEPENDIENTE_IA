8e6b3ac770dac3e243e0cf8db2a5f730eb682f38
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-22 04:59:34 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -9,11 +9,11 @@ let handler = async (m, { conn }) => {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
         let txt = ` –  *⫷ 𝕃 𝔼 𝕍 𝔼 𝕃 𝕌 ℙ⫸-⫷𝕌 𝕊 𝔼 ℝ ⫸*\n\n`
 	    txt += `⋉🔆▰▰▰🔥▰▰▰🔆⫸\n`
-	    txt += `▮ ▿▿▿▿▿▿▿▿▿▿▿▿▿\n`
+	    txt += `▮ ▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿▿\n`
             txt += `▮🔶 *Nombre* : ${name}\n`
             txt += `▮🔶 *Nivel* : ${user.level}\n`
             txt += `▮🔶 *XP* : ${user.exp - min}/${xp}\n`
-	    txt += `▮▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵\n`
+	    txt += `▮▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵▵\n`
 	    txt += `▮▰▰▰▰▰▰▰▰▰⫸\n`
             txt += `▮◈Te falta *${max - user.exp}* de *💫 XP* para \n` 
 	    txt += `▮         subir de nivel \n`
