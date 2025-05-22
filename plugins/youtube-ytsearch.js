e25abf0b00f60a490b2b4355e0c6a5fffe276356
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-22 00:33:31 -0500

    youtube busqueda

--- plugins/youtube-ytsearch.js
+++ plugins/youtube-ytsearch.js
@@ -1,21 +1,21 @@
 import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-    if (!text) return m.reply('[ ✰ ] Ingresa el título de un video o canción de *YouTube*.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Mc Davo - Debes De Saber`)
+    if (!text) return m.reply('💙 Ingresa el título de un video o canción de *YouTube*.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Hatsune Miku World is mine`)
     await m.react('🕓')
     try {
     let results = await Starlights.ytsearch(text)
     if (!results || !results.length) return conn.reply(m.chat, `No se encontraron resultados.`, m, rcanal)
     let img = await (await fetch(`${results[0].thumbnail}`)).buffer()
-    let txt = '`乂  Y T  -  S E A R C H`'
+    let txt = '`🔶  ⫷ 𝙔𝙏 - 𝙎𝙀𝘼𝙍𝘾𝙃 ⫸  🔶`'
     results.forEach((video, index) => {
         txt += `\n\n`
-        txt += `	✩  *Nro* : ${index + 1}\n`
-        txt += `	✩  *Titulo* : ${video.title}\n`
-        txt += `	✩  *Duración* : ${video.duration}\n`
-        txt += `	✩  *Publicado* : ${video.published}\n`
-        txt += `	✩  *Autor* : ${video.author}\n`
-        txt += `	✩  *Url* : ${video.url}`
+        txt += `	🎤  *Nro* : ${index + 1}\n`
+        txt += `	🎤  *Titulo* : ${video.title}\n`
+        txt += `	🎤  *Duración* : ${video.duration}\n`
+        txt += `	🎤  *Publicado* : ${video.published}\n`
+        txt += `	🎤  *Autor* : ${video.author}\n`
+        txt += `	🎤  *Url* : ${video.url}`
     })
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 await m.react('✅')
