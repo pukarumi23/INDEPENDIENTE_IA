e11a0bdf5b69dfeca3db60bb3de35f4ad2f91543
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-22 23:26:48 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/tools-toimg.js
+++ plugins/tools-toimg.js
@@ -1,22 +1,15 @@
 import { webp2png } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
-const notStickerMessage = `🔶 Responde a un *Sticker.*`
-if (!m.quoted) return conn.reply(m.chat, notStickerMessage, m, rcanal)
+const notStickerMessage = `🔶 Hubo un Error *${usedPrefix + command}*`
+//if (!m.quoted) return m.reply(notStickerMessage)
 const q = m.quoted || m
-let mime = q.mediaType || ''
-if (!/sticker/.test(mime)) return conn.reply(m.chat, notStickerMessage, m, rcanal)
-await m.react('🕓')
-try {
-let media = await q.download()
+const mime = q.mediaType || ''
+if (!/sticker/.test(mime)) return m.reply(notStickerMessage)
+const media = await q.download()
 let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-await conn.sendFile(m.chat, out, 'thumbnail.jpg', listo, m)
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
-handler.help = ['toimg *<sticker>*']
-handler.tags = ['sticker', 'tools']
-handler.command = ['toimg', 'jpg', 'aimg'] 
-handler.register = true
-
+await conn.sendFile(m.chat, out, 'error.png', null, m)
+}
+handler.help = ['toimg (reply)']
+handler.tags = ['sticker']
+handler.command = ['toimg', 'img', 'jpg']
 export default handler
