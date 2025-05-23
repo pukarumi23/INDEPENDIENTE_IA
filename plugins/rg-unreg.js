9e78eeb8a919f1ad7310f590811461137d2de599
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-22 22:33:48 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -1,11 +1,11 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) return m.reply(`🚩 Ingresa tu número de serie junto al comando.`)
+  if (!args[0]) return m.reply(`🔶 Ingresa tu número de serie junto al comando.`)
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) return m.reply('🚩 Número de serie incorrecto')
+  if (args[0] !== sn) return m.reply('🔶 Número de serie incorrecto')
   user.registered = false
-  m.reply(`🚩 Registro eliminado.`)
+  m.reply(`🔶 Registro eliminado.`)
 }
 handler.help = ['unreg'] 
 handler.tags = ['rg']
