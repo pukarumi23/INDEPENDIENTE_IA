ea81701071ae5d75bf4f0658ee93530be79d0fe8
pukarumi23 <edwinsantacruzh@gmail.com>
2025-05-23 00:51:14 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `🚩 Chat Baneado con exito.`, m, rcanal)
+    await conn.reply(m.chat, `🔶 Chat Baneado con exito.\n ⫸Independiente se fue a dormir (^人^)`, m, rcanal)
     await m.react('✅')
 }
 handler.help = ['banearbot']
 handler.tags = ['group']
-handler.command = ['banearbot', 'banchat']
+handler.command = ['banearbot', 'banchat''dormir', ]
 handler.group = true 
 export default handler
