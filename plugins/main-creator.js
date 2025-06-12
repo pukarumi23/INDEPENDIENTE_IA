64c9226e5bbbd114ead7e073aeebbf39577ce775
pukarumi23 <edwinsantacruzh@gmail.com>
2025-06-11 23:28:51 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;(ㅎㅊDEPOOLㅊㅎ);;\nFN:(ㅎㅊDEPOOLㅊㅎ)\nORG:(ㅎㅊDEPOOLㅊㅎ)\nTITLE:\nitem1.TEL;waid=51988514570:51988514570\nitem1.X-ABLabel:(ㅎㅊDEPOOLㅊㅎ)\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:(ㅎㅊDEPOOLㅊㅎ)\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: '(ㅎㅊDEPOOLㅊㅎ)', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;(CHASKI);;\nFN:(CHASKI)\nORG:(CHASKI)\nTITLE:\nitem1.TEL;waid=51939508653:51939508653\nitem1.X-ABLabel:(CHASKI)\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:(CHASKI)\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: '(CHASKI)', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
