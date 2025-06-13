509d4219071265b0189ec1f4acdb336b480189f6
pukarumi23 <edwinsantacruzh@gmail.com>
2025-06-12 22:11:33 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/fun-personalidad.js
+++ plugins/fun-personalidad.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, command, text }) => {
 	
-    if (!text) return m.reply(`💙 Ingresa un nombre justo al comando.`)
+    if (!text) return m.reply(`🔶 Ingresa un nombre justo al comando.`)
 	
   let personalidad = `
-┏━━°🌱❬ *PERSONALIDAD}* ❭🥢°━━┓
+┏━━°🔆❬ *PERSONALIDAD}* ❭🥢°━━┓
 *┃*
 *┃• Nombre* : ${text}
 *┃• Buena Moral* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
