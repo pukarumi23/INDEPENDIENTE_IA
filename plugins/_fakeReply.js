76531ff7ab6db9d89fd91c86dc6a97beae252bbe
pukarumi23 <edwinsantacruzh@gmail.com>
2025-06-13 02:12:30 -0500

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -5,11 +5,11 @@ let img = await (await fetch(`https://tinyurl.com/2898ndat`)).buffer()
 
   const canales = [
     {
-      id: "124361163993209@lid",
+      id: "120363418348675041@newsletter",
       nombre: "🔶INDEPENDIENTE BOT🔶",
     },
     {
-      id: "124361163993209@lid",
+      id: "120363418348675041@newsletter",
       nombre: "🔶INDEPENDIENTE BOT🔶",
     },
   ]
