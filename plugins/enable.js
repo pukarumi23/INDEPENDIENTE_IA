6bd32fa31560f7241e0e0302b18996e898d0e687
pukarumi23 <edwinsantacruzh@gmail.com>
2025-06-15 15:40:21 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/enable.js
+++ plugins/enable.js
@@ -16,12 +16,12 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
     case 'bv':
     case 'bienvenida':
       if (!m.isGroup) {
-        if (!isOwner) {
+        if (!isOwner,isOwner) {
           global.dfail('group', m, conn)
           throw false
         }
-      } else if (!isAdmin) {
-        global.dfail('admin', m, conn)
+      } else if (!isAdmin,isOwner) {
+        global.dfail('admin','Owner', m, conn)
         throw false
       }
       chat.bienvenida = isEnable
