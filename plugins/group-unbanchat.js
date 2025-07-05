57dcea42a5d229650031907654246ded0c76c42d
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-05 02:01:30 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '💙 Bot activo en este grupo.', m, rcanal)
+    await conn.reply(m.chat, '🔶independiente se desperto.', m, rcanal)
     await m.react('✅')
 }
 handler.help = ['desbanearbot']
 handler.tags = ['group']
-handler.command = ['desbanearbot', 'unbanchat']
+handler.command = ['desbanearbot','dn', 'unbanchat']
 handler.group = true 
 export default handler
