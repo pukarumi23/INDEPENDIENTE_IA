7e940c62f6750cedeca35d65c9650a7b4f21cecd
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-07 01:46:50 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/nable-antiprivado.js
+++ plugins/nable-antiprivado.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
    let setting = global.database.settings[conn.user.jid] || {}
    if (args[0] === 'on') {
-      if (setting.antiPrivate) return conn.reply(m.chat, 'Anti-Privado ya esta Activada.', m)
+      if (setting.antiPrivate) return conn.reply(m.chat, 'Anti-Privado ya esta ActivadO.', m)
       setting.antiPrivate = true
-      await conn.reply(m.chat, '💙 Anti-Privado activada para este Bot.', m)
+      await conn.reply(m.chat, '🔶 Anti-Privado activadO para este Bot.', m)
    } else if (args[0] === 'off') {
-      if (!setting.antiPrivate) return conn.reply(m.chat, 'Anti-Privado ya esta Desactivada.', m)
+      if (!setting.antiPrivate) return conn.reply(m.chat, 'Anti-Privado ya esta DesactivadO.', m)
       setting.antiPrivate = false
-      await conn.reply(m.chat, '💙 Anti-Privado desactivada para este Bot.', m)
+      await conn.reply(m.chat, '🔶 Anti-Privado desactivadO para este Bot.', m)
    } else {
       await conn.reply(m.chat, `*Configurar Anti-Privado*. Escriba on para activar y off para Desactivar.`, m)
    }
