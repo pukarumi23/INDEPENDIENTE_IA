47803af7eb347e288d69a1ca6ff57d5cbcd98984
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-07 01:02:20 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/group-resetLink.js
+++ plugins/group-resetLink.js
@@ -1,6 +1,6 @@
 let handler = async(m, { conn }) => {
 let revoke = await conn.groupRevokeInvite(m.chat)
-await conn.reply(m.chat, `💙 Se restableció con éxito el link del grupo.\n*-* Link Nuevo: ${'https://chat.whatsapp.com/' + revoke}`, m)}
+await conn.reply(m.chat, `🔶 Se restableció con éxito el link del grupo.\n*-* Link Nuevo: ${'https://chat.whatsapp.com/' + revoke}`, m)}
 handler.help = ['resetlink']
 handler.tags = ['group']
 handler.command = ['revoke', 'resetlink', 'anularlink'] 
