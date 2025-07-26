9bf9433f6ca31fe98e814745906e2a79def074ea
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-26 18:04:16 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-deposit.js
+++ plugins/rpg-deposit.js
@@ -2,22 +2,22 @@
 
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('💙 Ingresa la cantidad de *🌱 Cebollines* que deseas Depositar.')
-   if ((args[0]) < 1) return m.reply('💙 Ingresa una cantidad válida de *🌱 Cebollines.')
+   if (!args[0]) return m.reply('🔶 Ingresa la cantidad de *🪙 Intis* que deseas Depositar.')
+   if ((args[0]) < 1) return m.reply('🔶 Ingresa una cantidad válida de *🪙 Intis.')
    if (args[0] == 'all') {
       let count = parseInt(user.limit)
       user.limit -= count * 1
       user.bank += count * 1
-      await m.reply(`Depositaste *${count} 🌱 Cebollines* al Banco.`)
+      await m.reply(`Depositaste *${count} 🪙 Intis* al Banco.`)
       return !0
    }
-   if (!Number(args[0])) return m.reply('💙 La cantidad deve ser un Numero.')
+   if (!Number(args[0])) return m.reply('🔶 La cantidad deve ser un Numero.')
    let count = parseInt(args[0])
-   if (!user.limit) return m.reply('No tienes *🌱 Cebollines* en la Cartera.')
-   if (user.limit < count) return m.reply(`Solo tienes *${user.limit} 🌱 Cebollines* en la Cartera.`)
+   if (!user.limit) return m.reply('No tienes *🪙 Intis* en la Cartera.')
+   if (user.limit < count) return m.reply(`Solo tienes *${user.limit} 🪙 Intis* en la Cartera.`)
    user.limit -= count * 1
    user.bank += count * 1
-   await m.reply(`Depositaste *${count} 🌱 Cebollines* al Banco.`)
+   await m.reply(`Depositaste *${count} 🪙 Intis* al Banco.`)
 }
 
 handler.help = ['depositar']
