902d1aa679bba90b1d8bc35af789f1a4333c9e01
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-26 11:56:46 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -1,11 +1,9 @@
-
-
 let handler = async (m, {conn, usedPrefix}) => {
    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
    if (who == conn.user.jid) return m.react('✖️')
    if (!(who in global.db.data.users)) return m.reply(`*El usuario no se encuentra en mi base de datos*`)
    let user = global.db.data.users[who]
-   await m.reply(`${who == m.sender ? `╔═════.·:·.🏦.·:·.═════╗\n *${user.bank} 🪙 Intis* \n═════.·:·.🏦.·:·.═════` : `El usuario @${who.split('@')[0]} tiene *${user.bank} 🪙 Intis* en el Banco`}`, null, { mentions: [who] })
+   await m.reply(`${who == m.sender ? `╔═════.·:·.🏦.·:·.═════╗\n ⫸🔁 𝘽𝘼𝙉𝘾𝙊 ⛓ \n⫸ *${user.bank} 🪙 𝙄𝙉𝙏𝙄𝙎* \n╚═════.·:·.🌀.·:·.═════╝` : `El usuario @${who.split('@')[0]} tiene *${user.bank} 🪙 Intis* en el Banco`}`, null, { mentions: [who] })
 }
 
 handler.help = ['bank']
