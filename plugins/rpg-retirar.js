3c62aadbdbe83f39634abad6f6ebbb9c712a39a4
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-27 11:17:40 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -2,21 +2,21 @@
 
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('💙 Ingresa la cantidad de *🌱 Cebollines* que deseas Retirar.')
+   if (!args[0]) return m.reply('🔶 Ingresa la cantidad de *🪙 Intis* que deseas Retirar.')
    if (args[0] == 'all') {
       let count = parseInt(user.bank)
       user.bank -= count * 1
       user.limit += count * 1
-      await m.reply(`💙 Retiraste *${count} 🌱 Cebollines* del Banco.`)
+      await m.reply(`🔶 Retiraste *${count} 🪙 Intis* del Banco.`)
       return !0
    }
-   if (!Number(args[0])) return m.reply('💙 La cantidad deve ser un Numero.')
+   if (!Number(args[0])) return m.reply('🔶 La cantidad deve ser un Numero.')
    let count = parseInt(args[0])
-   if (!user.bank) return m.reply('No tienes *🌱 Cebollines* en el Banco.')
-   if (user.bank < count) return m.reply(`Solo tienes *${user.bank} 🌱 Cebollines* en el Banco.`)
+   if (!user.bank) return m.reply('No tienes *🪙 Intis* en el Banco.')
+   if (user.bank < count) return m.reply(`Solo tienes *${user.bank} 🪙 Intis* en el Banco.`)
    user.bank -= count * 1
    user.limit += count * 1
-   await m.reply(`💙 Retiraste *${count} 🌱 Cebollines* del Banco.`)
+   await m.reply(`🔶 Retiraste *${count} 🪙 Intis* del Banco.`)
 }
 
 handler.help = ['retirar']
