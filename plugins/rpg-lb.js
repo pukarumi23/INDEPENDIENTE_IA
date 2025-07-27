6d01b6d54cf2a370d86ef4483a2a8fda40a701ca
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-27 10:39:18 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -11,22 +11,22 @@ let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0])
 let text = `
 ╭───═[ 🔶 *Top ${len} 🪙 Cebollines* 🔶 ]═────⋆
 │╭───────────────···
-0│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-1│ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} 🪙*`).join`\n▮⫸ `}
+◆│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+◇│ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} 🪙*`).join`\n▮⫸ `}
 │╰────────────────···
 ╰───────────═┅═──────────
 
 ╭─═[ 🔶 *TOP ${len} XP 💫* 🔶 ]═──⋆
 │╭───────────────···
-0│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-1│ ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} 💫*`).join`\n▮⫸ `}
+◆│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+◇│ ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} 💫*`).join`\n▮⫸ `}
 │╰────────────────···
 ╰───────────═┅═──────────
 
 ╭─═[ 🔶 *Top ${len} Nivel 📈* 🔶 ]═──⋆
 │╭───────────────···
-2│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-│ ${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Nivel ${level} 📈*`).join`\n▮⫸ `}
+◆│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+◇│ ${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Nivel ${level} 📈*`).join`\n▮⫸ `}
 │╰────────────────···
 ╰───────────═┅═──────────`.trim()
   m.reply(text, null, { mentions: conn.parseMention(text) })
