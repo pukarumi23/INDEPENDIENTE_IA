2780fa4ff57fc0b29b78a2664e2ee80984de1094
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-27 14:23:29 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -8,11 +8,11 @@ let handler = async (m, { conn, command, args }) => {
     global.db.data.users[m.sender].limit += count
     conn.reply(m.chat, `╭────═[ *R P G  -  S H O P* ]═─────⋆
 │╭───────────────···
-││0 *Compra* : + ${count} 🌱 Cebollines 
+││0 *Compra* : + ${count} 🪙 Intis 
 ││1 *Costo* : -${xpperlimit * count} 💫 XP
 │╰────────────────···
 ╰───────────═┅═──────────`, m, rcanal)
-  } else conn.reply(m.chat, `💙 Lo siento, no tienes suficientes *🌱 XP* para comprar *${count} 🌱 Cebollines.*`, m, rcanal)
+  } else conn.reply(m.chat, `🔶 Lo siento, no tienes suficientes *🪙 XP* para comprar *${count} 🪙 Intis.*`, m, rcanal)
 }
 handler.help = ['buycoins', 'buyall']
 handler.tags = ['rpg']
