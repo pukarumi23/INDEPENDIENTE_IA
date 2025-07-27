f0236202175c8f6f39f7f00e85bfc96aa716b8f0
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-27 14:42:35 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/rpg-stars.js
+++ plugins/rpg-stars.js
@@ -5,13 +5,13 @@ let handler = async (m, {conn, usedPrefix}) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
     let name = conn.getName(who)
-    if (!(who in global.db.data.users)) return conn.reply(m.chat, '💙 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
-    let img = await (await fetch(`https://w7.pngwing.com/pngs/531/1011/png-transparent-hatsune-miku-vocaloid-chibi-holography-anime-hatsune-miku-fictional-characters-manga-chibi.png`)).buffer()
-    let txt = ` –  *C E B O L L I N E S  -  U S E R*\n\n`
-        txt += `┌  💙  *Nombre* : ${user.name}\n`
-        txt += `│  💙  *Cebollines* : ${toNum(user.limit)} ( *${user.limit}* )\n`
-        txt += `│  💙  *Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
-        txt += `└  💙  *XP* : ${toNum(user.exp)} ( *${user.exp}* )`
+    if (!(who in global.db.data.users)) return conn.reply(m.chat, '🔶 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
+    let img = await (await fetch(`https://img.freepik.com/vector-premium/cielo-anime-puesta-sol-fondo-nubes-puesta-sol_8071-63722.jpg`)).buffer()
+    let txt = `╔═════⊰ 🧅  𝙄𝙉𝙏𝙄𝙎 - 𝙐𝙎𝙀𝙍 ⊱═════╗ \n\n`
+        txt += `💠 𝙉𝙊𝙈𝘽𝙍𝙀 : ${user.name}\n`
+        txt += `💰 𝙄𝙉𝙏𝙄𝙎    : ${toNum(user.limit)} ( *${user.limit}* )\n`
+        txt += `🏦 𝘽𝘼𝙉𝘾𝙊  : ${toNum(user.bank)} ( *${user.bank}* )\n`
+        txt += `📈 𝙓𝙋      : ${toNum(user.exp)} ( *${user.exp}* )`
     let mentionedJid = [who]
         
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
