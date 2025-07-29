80a3494e8aac147c67703b4a4a02e4e722f194cb
pukarumi23 <edwinsantacruzh@gmail.com>
2025-07-29 01:51:11 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/fun-aburrido.js
+++ plugins/fun-aburrido.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn }) => {
     m.react('🔥'); // Reacción al mensaje
 
     // Mensaje indicando que la persona está aburrida
-    let str = `▰▰▰▰▰🔶▰▰▰▰▰▰ \n ${name2} está aburrido/a🥱. \n ▰▰▰▰▰▰▰▰▰▰▰▰▰`.trim();
+    let str = `༺══✦༻༺══✦༻❀༺✦══༻༺══✦༻ \n ${name2} está aburrido/a🥱. \n༺══✦༻༺══✦༻❀༺✦══༻༺══✦༻`.trim();
 
     // Si es un grupo, seleccionamos un video aleatorio y lo enviamos
     if (m.isGroup){
